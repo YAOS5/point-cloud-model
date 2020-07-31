49d8fa7c749cef3476efac6efada7f9ee549b712
Sheepiyoo <yeohzx99@gmail.com>
2020-08-01 02:59:02 +1000

    Add template unprojection file

---
+++ Squeezeseg/unproject.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+import numpy as np
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('prediction')
+    args = parser.parse_args()
+
+
+    
